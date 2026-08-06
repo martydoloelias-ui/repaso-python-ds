7f7d020971ffa4c4a25edd692d749282fe28b0f4
Martina dolores Elias <martydoloelias@gmail.com>
2026-08-06 01:03:39 -0300

    Update analisis_basico.py

--- analisis_basico.py
+++ analisis_basico.py
@@ -4,16 +4,16 @@ precio = 25999.90
 cantidad_stock = 15
 tiene_descuento = True
 
-print(f"Producto: {nombre_producto} | Precio: ${precio}  |  Stock: {cantidad_stock}
+print(f"Producto: {nombre_producto} | Precio: ${precio}  |  Stock: {cantidad_stock}")
 
 #Tarea 2: Logica de negocio (Bucle + Condicional)
 precios = [45.40, 120.00, 99.99, 250.30, 80.00]
 
 for precio_item in precios:
-if precio_item > 100:
-print(f"${precio_item} -> Caro")
-else: 
-print(f"${precio_item} -> Economico")
+    if precio_item > 100:
+         print(f"${precio_item} -> Caro")
+    else: 
+         print(f"${precio_item} -> Economico")
 
 #Tarea 3: Estructura de Datos Compleja
 almacen = {
@@ -28,10 +28,10 @@ print(f"Stock de Persiana Americana: {almacen['Persiana Americana']}")
 
 #Tarea 4: Modularizacion con funciones
 def resumen_estadistico(lista_numeros):
-suma_total = sum(lista_numeros)
-cantidad = len(lista_numeros)
-promedio = suma_total / cantidad
-return {"suma_total": suma_total, "promedio": promedio, "cantidad_elementos": cantidad}
+    suma_total = sum(lista_numeros)
+    cantidad = len(lista_numeros)
+    promedio = suma_total / cantidad
+    return {"suma_total": suma_total, "promedio": promedio, "cantidad_elementos": cantidad}
 
 resultado = resumen_estadistico(precios)
 print(resultado)
